a3cd25b85f52b7a646bff8411e045ef08fecf4f7
Priya Talreja <priyatalreja@Priyas-MacBook-Pro.local>
2017-05-28 12:36:57 +0530

    Initial Commit

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,46 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let firstNumber = 5
+
+//firstNumber = 2
+//above commented line error: cannot assign to value: 'firstNumber' is a 'let' constant
+
+var a = 2
+var b = 5
+
+var answer = a + b
+
+a = 7
+
+var updatedAnswer = a + b
+
+var message: String = "Hello Developers!"
+
+var no1: Int = 1
+
+//no1 = 2.2  /* error: cannot assign value of type 'Double' to type 'Int'*/
+
+var no2 = 2
+
+var addition = no1 + no2
+
+var no3 = 3.5
+
+//var add = no1 + no3  /* error: binary operator '+' cannot be applied to operands of type 'Int' and 'Double' */
+
+print(no1 * no2)
+
+var counter = 100
+counter -= 1
+counter += 5
+counter *= 2
+counter /= 5
+
+
+
+
+
+
+
